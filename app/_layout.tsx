612b71b9c48ae4eff761d8875df0315d17649a74
Andrew Youngtaek Song <y_song179344@fanshaweonline.ca>
2024-08-01 20:53:22 -0400

    Added Tab.Screen with Book and Borrowed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,33 @@
-import { Stack } from "expo-router";
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Tabs } from 'expo-router';
 
 export default function RootLayout() {
   return (
+    /*
     <Stack>
       <Stack.Screen name="index" />
     </Stack>
+    */
+    <Tabs>
+      <Tabs.Screen
+        name="Home"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="Borrowed"
+        options={{
+          title: 'Borrowed',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'book' : 'book-outline'} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
